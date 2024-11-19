5a0848be4adb093def3667502eee19e9d8c893b8
Alexsandro <alex_gaiotte@hotmail.com>
2024-11-19 20:33:45 -0300

    Questão3 sem progresso

--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from todos.views import TodoListView
+from todos.views import TodoListView, TodoCreateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
 urlpatterns = [
- path('', TodoListView.as_view(), name='todo_list')
+    path("", TodoListView.as_view(), name="todo_list"),
+    path("create/", TodoCreateView.as_view(), name="todo_create"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -2,10 +2,12 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
 from .models import Todo
 
-class TodoListView(ListView):
- model = Todo
 
- class TodoCreateView(CreateView):
-     model = Todo
-     fields = ['title','deadline']
-     success_url = reverse_lazy('todo_list')
+class TodoListView(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
