620be021a2d32677314f2d393ccd01610ffe8ef3
Alexsandro <alex_gaiotte@hotmail.com>
2024-11-19 19:27:57 -0300

    Questão1

---
+++ todos/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Todo
+
+class TodoListView(ListView):
+ model = Todo
+
+
